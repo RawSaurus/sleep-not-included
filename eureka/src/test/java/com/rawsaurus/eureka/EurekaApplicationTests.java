a172cee4baa8194b12595ad4e294345516bb4cb6
RawSaurus <mirino.bella@gmail.com>
2026-01-26 15:31:50 +0100

    service discovery

---
+++ eureka/src/test/java/com/rawsaurus/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawsaurus.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
